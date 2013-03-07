a8b44863a752409aca340f2b6c8df12bf397fbd7
4l2l3 <sakatai@gmail.com>
2013-03-07 15:00:49 -0500

    do_images() begun

--- blah.py
+++ blah.py
@@ -1,57 +1,52 @@
 #!/usr/bin/python
 import re
 
-wiki_pages = ['ABySS', 'ACLs', 'ADS', 'Ab-Init', 'Ansys', 'ApacheNutch', 'AutoDock', 'Blast', 'CDBurning', 'CMAQ-4.5.1', 'CMAQ-4.6', 'CMAQ-4.7', 'Cadence', 'CamelCase', 'CirceConnect', 'CirceDataAccess', 'CirceDataManagement', 'CirceDesktop', 'CirceHardware', 'CirceLayout', 'Cubit', 'DataBackup', 'DeveloperPortal', 'DevelopmentTools', 'Downtime2008', 'Downtime2009', 'ESPRIT', 'FAQ', 'FemlabUser', 'Fidap', 'GMT', 'GaussView', 'Gaussian', 'GibsonSpecs', 'GrADS', 'Grass', 'Gromacs', 'Gulp', 'HFSS', 'HPCPack2008R2Client', 'HPlatforms', 'Hadoop', 'HesiodConnect', 'IDLUser', 'InfiniBand', 'InterMapTxt', 'InterTrac', 'InterWiki', 'Iso2Mesh', 'JobRequirements', 'LAMMPS', 'MEGA', 'MM5', 'MM5OnIrce', 'MOVES', 'MPB', 'MPIBlast', 'MPP', 'MSModeling', 'MapleUser', 'MathemCluster', 'MathemUser', 'MatlabCluster', 'MatlabUser', 'Maya', 'Meep', 'Meme', 'Modules', 'MrBayes', 'MyriExp', 'NeedHPC', 'PGIInstall', 'PageTemplates', 'ParallelEnvironments', 'Quotas', 'RecentChanges', 'RestoreUtility', 'Rmpi', 'Run', 'SAS', 'SandBox', 'SoftPortalMockup', 'SoftwarePortal', 'SunblastData', 'Synopsys', 'TeraChem', 'TitleIndex', 'TracAccessibility', 'TracAdmin', 'TracBackup', 'TracBrowser', 'TracCgi', 'TracChangeset', 'TracEnvironment', 'TracFastCgi', 'TracFineGrainedPermissions', 'TracGuide', 'TracImport', 'TracIni', 'TracInstall', 'TracInterfaceCustomization', 'TracLinks', 'TracLogging', 'TracModPython', 'TracModWSGI', 'TracNavigation', 'TracNotification', 'TracPermissions', 'TracPlugins', 'TracQuery', 'TracReports', 'TracRepositoryAdmin', 'TracRevisionLog', 'TracRoadmap', 'TracRss', 'TracSearch', 'TracStandalone', 'TracSupport', 'TracSyntaxColoring', 'TracTickets', 'TracTicketsCustomFields', 'TracTimeline', 'TracUnicode', 'TracUpgrade', 'TracWiki', 'TracWorkflow', 'UsingComplexes', 'WEKA', 'Webdav', 'WhatIsHPC', 'WhatSoftware', 'WhoResources', 'WikiDeletePage', 'WikiFormatting', 'WikiHtml', 'WikiMacros', 'WikiNewPage', 'WikiPageNames', 'WikiProcessors', 'WikiRestructuredText', 'WikiRestructuredTextLinks', 'WikiStart', 'WinHPCJobStatus', 'WindowsISO', 'XWin32Install', 'XmingInstall', 'abinit', 'accountInfo', 'cp2k', 'dalton', 'desnex', 'devenv', 'dlpoly', 'dock6', 'esmf', 'fastxToolkit', 'fdtd', 'feram', 'gpuJobs', 'gridEngineInter', 'gridEngineIntro', 'gridEnginePolicy', 'gridEngineQueue', 'gridEngineRuntime', 'gridEngineStatus', 'gridEngineTechn', 'gridEngineUsers', 'here', 'jmol', 'libNuma', 'molden', 'namd', 'nwchem', 'oases', 'openfoam', 'openmm', 'proj', 'qe', 'siesta', 'testpage', 'titan2d', 'transabyss', 'vapor', 'vasp', 'velvet', 'vmd', 'vnc-class', 'vpnClient', 'websites', 'wrf']
-
-text_in = open("links",'r').read()
+text_in = open("test_cases/images",'r').read()
 text_out = text_in
-#TRAC		mailto:someone@foo.bar
-email_pat = re.compile('mailto:.+@[A-Za-z0-9.-]+\.[a-z]{2,}')
-all_mailto = email_pat.findall(text_out)
-for mailto in all_mailto:
-	text_out = text_out.replace(mailto,mailto[len("mailto:"):])
-	#REDMINE	someone@foo.bar
 
-#TRAC		[mailto:someone@foo.bar "Email someone"]
-mailto_pat = re.compile('\[mailto:.+@[A-Za-z0-9.-]+\.[a-z]{2,} ".+"\]')
-all_mailtos = mailto_pat.findall(text_out)
-for single in all_mailtos:
-	email_someone = re.compile('".+"').search(single) #var name refers to quoted text
-	hd = email_someone.start()
-	tl = email_someone.end()
+#TRAC		[[Image(image_url)]]
+#REDMINE	!image_url! 
+#TRAC		[[Image(attached_image.png)]]	
+#REDMINE	!attached_image.png!	
+image_loc = re.compile('\[\[Image\(.{4,}\]\]\)')
+images = image_loc.findall(text_out)
+for image in images:
+	loc = image[len("[[Image("):]
+	text_out = text_out.replace(image, "!"+loc+"!")
 
-	email_addr = single[len("[mailto:"):hd-1] #instead of 8, I put in len(...) for clarity
-	#REDMINE	"Email someone":mailto:someone@foo.bar
-	new_mailto = email_someone.group()+":mailto:"+email_addr
-	text_out = text_out.replace(single, new_mailto)
+#SPECIAL ATTRIBUTES
+img_r = re.compile('\[\[Image\(.{4,}, [a-z\-]+=.+\]\]\)')
+imgs = img_r.findall(text_out)
+for img in imgs:
+	#search only finds first occurence
+	img_name = re.compile('[^, ]+').search(img[len("[[Image("):]).group()
 
-#TRAC		[http://whatever.com/ "Title"]
-titled_url = re.compile('\[https?://.{4,} ".+"\]') #went the lax route and allowed URLs and titles to be of any format
-all_turls = titled_url.findall(text_out)
-for turl in all_turls:
-	title = re.compile('".+"').search(turl)
-	hd = title.start()
-	tl = title.end()
-	url = turl[1:hd-1] #first one cuts off leading "["
-	#REDMINE	"Title":http://whatever.com
-	new_turl = title.group() + ":" + url
-	text_out = text_out.replace(turl,new_turl)
+	#slicing after "[[Image(name, ", +2 is for space and comma 
+	prefix_num = len("[[Image(")+len(img_name) +2 
 
-#TRAC		WikiPageName
-words = text_out.split() 
-for word in words:
-	if wiki_pages.count(word)!=0:#we're good and we link it
-		text_out = text_out.replace(word, "[["+ word +"]]")
-#REDMINE	[[WikiPageName]]
+	#TRAC		[[Image(photo.jpg, align=right)]]
+	#REDMINE	!>image_url! 
+	if(img[prefix_num:len(img)-3] =="align=right" ):
+		text_out = text_out.replace(img,"!>"+img_name+"!"
 
-#TRAC		[wiki:WikiPageName "Title"]
-titled_wiki = re.compile('\[wiki:[A-Za-z0-9.-]+ ".+"\]')
-all_twiki = titled_wiki.findall(text_out)
-for twiki in all_twiki:
-	title = re.compile('".+"').search(twiki)
-	hd = title.start()
-	page_name = twiki[len("[wiki:"):hd-1]
-	#REDMINE	[[WikiPageName|Title]]
-	new_twiki = "[["+page_name+"|"+title.group()+"]]"
-	text_out = text_out.replace(twiki,new_twiki)
+	#TRAC		[[Image(photo.jpg, alt='Image title')]]
+	#REDMINE	!image_url(Image title)! displays an image with an alt/title attribute	
+	elif(img[prefix_num:prefix_num+4]=="alt="):
+		alt_txt = img[ prefix_num+4 : len(img)-3]
+		text_out = text_out.replace(img, "!img_name("+alt_txt+")!"
+
+	#TRAC		[[Image(photo.jpg, 120px)]]               
+	#REDMINE	!{width: 100%}attached_image.png!	
+	elif(img[len(img)-4:len(img)-3]=="x"): #short for px
+		
+	elif(img[len(img)-4:len(img)-3]=="%"):
+	#TRAC		[[Image(photo.jpg, key=value)]]
+	#REDMINE	!{key: value}attached_image.png!
+	else: 	
+	
+
+		img_n_keys = re.compile('\[\[Image\(\)')
+
+
+
 print text_out
