e172096aba0e6b4c41d0aac6c8d59589e591c4d6
4l2l3 <sakatai@gmail.com>
2013-03-11 12:14:57 -0400

    i was missing ending ')' for all the .replace() calls

--- blah.py
+++ blah.py
@@ -27,21 +27,22 @@ for img in imgs:
 	#TRAC		[[Image(photo.jpg, align=right)]]
 	#REDMINE	!>image_url! 
 	if (img[prefix_num:len(img)-3] =="align=right" ):
-		text_out = text_out.replace(img,"!>"+img_name+"!"
-	
-	elif ((img[prefix_num:prefix_num+4])=="alt="):#TRAC		[[Image(photo.jpg, alt='Image title')]]
-		alt_txt = img[ prefix_num+4 : len(img)-3]#REDMINE	!image_url(Image title)! displays an image with an alt/title attribute	
-		text_out = text_out.replace(img, "!"+img_name+"("+alt_txt+")!"
+		text_out = text_out.replace(img,"!>"+img_name+"!")
+	#TRAC		[[Image(photo.jpg, alt='Image title')]]
+	#REDMINE	!image_url(Image title)! displays an image with an alt/title attribute	
+	elif ((img[prefix_num:prefix_num+4])=="alt="):
+		alt_txt = img[ prefix_num+4 : len(img)-3]
+		text_out = text_out.replace(img, "!"+img_name+"("+alt_txt+")!")
 
 	#TRAC		[[Image(photo.jpg, 120px)]]               
 	#REDMINE	!{width: 100%}attached_image.png!	
 	elif (img[len(img)-5:len(img)-3]=="px"): 
 		pixel_size = img[prefix_num:len(img)-5]
-		text_out = text_out.replace(img, "!{width: "+pixel_size+"%}"+img_name+"!"
+		text_out = text_out.replace(img, "!{width: "+pixel_size+"%}"+img_name+"!")
 		
 	elif (img[len(img)-4:len(img)-3]=="%"):
 		pixel_size = img[prefix_num:len(img)-4]
-		text_out = text_out.replace(img, "!{width: "+pixel_size+"px}"+img_name+"!"
+		text_out = text_out.replace(img, "!{width: "+pixel_size+"px}"+img_name+"!")
 
 	#TRAC		[[Image(photo.jpg, key=value)]]
 	#REDMINE	!{key: value}attached_image.png!
@@ -56,7 +57,7 @@ for img in imgs:
 			key = key[:len(key)-1] #chop off '='
 			new_keypair = key+": "+val
 			new_keypairs.append(new_keypair)
-		text_out = text_out.replace(img,"!{"+ ", ".join([str(x) for x in new_keypairs])+"}"+img_name+"!"
+		text_out = text_out.replace(img,"!{"+ ", ".join([str(x) for x in new_keypairs])+"}"+img_name+"!")
 
 
 print text_out
