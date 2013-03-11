79821d77b5c6722b1d31aae628b2d65b1ec7b76d
4l2l3 <sakatai@gmail.com>
2013-03-11 11:42:18 -0400

    weird invalid syntax error on line 33 of blah.py

--- blah.py
+++ blah.py
@@ -26,27 +26,38 @@ for img in imgs:
 
 	#TRAC		[[Image(photo.jpg, align=right)]]
 	#REDMINE	!>image_url! 
-	if(img[prefix_num:len(img)-3] =="align=right" ):
+	if (img[prefix_num:len(img)-3] =="align=right" ):
 		text_out = text_out.replace(img,"!>"+img_name+"!"
-
 	#TRAC		[[Image(photo.jpg, alt='Image title')]]
 	#REDMINE	!image_url(Image title)! displays an image with an alt/title attribute	
-	elif(img[prefix_num:prefix_num+4]=="alt="):
+	elif ((img[prefix_num:prefix_num+4])=="alt="):
 		alt_txt = img[ prefix_num+4 : len(img)-3]
-		text_out = text_out.replace(img, "!img_name("+alt_txt+")!"
+		text_out = text_out.replace(img, "!"+img_name+"("+alt_txt+")!"
 
 	#TRAC		[[Image(photo.jpg, 120px)]]               
 	#REDMINE	!{width: 100%}attached_image.png!	
-	elif(img[len(img)-4:len(img)-3]=="x"): #short for px
+	elif (img[len(img)-5:len(img)-3]=="px"): 
+		pixel_size = img[prefix_num:len(img)-5]
+		text_out = text_out.replace(img, "!{width: "+pixel_size+"%}"+img_name+"!"
 		
-	elif(img[len(img)-4:len(img)-3]=="%"):
+	elif (img[len(img)-4:len(img)-3]=="%"):
+		pixel_size = img[prefix_num:len(img)-4]
+		text_out = text_out.replace(img, "!{width: "+pixel_size+"px}"+img_name+"!"
+
 	#TRAC		[[Image(photo.jpg, key=value)]]
 	#REDMINE	!{key: value}attached_image.png!
 	else: 	
-	
-
-		img_n_keys = re.compile('\[\[Image\(\)')
-
+		keypairs = re.compile('[A-Za-z]+=[^,]+').findall(img[prefix_num:])
+		new_keypairs = []
+		for keypair in keypairs:
+			match = re.compile('[a-zA-Z]+=').search(keypair)
+			hd = match.start()
+			key = match.group()
+			val = keypair[len(key):] #doing this in the middle also removes the '='
+			key = key[:len(key)-1] #chop off '='
+			new_keypair = key+": "+val
+			new_keypairs.append(new_keypair)
+		text_out = text_out.replace(img,"!{"+ ", ".join([str(x) for x in new_keypairs])+"}"+img_name+"!"
 
 
 print text_out
