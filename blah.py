4347f09085faa05725ea4f75cf7e8027b48ebf93
4l2l3 <sakatai@gmail.com>
2013-03-14 12:15:33 -0400

    top-level header RE wasn't grabbing header due to newline at end and use of \w

--- blah.py
+++ blah.py
@@ -1,24 +1,35 @@
 #!/usr/bin/python
 import re
 
-text_in = open("pages_in_wiki/oases",'r').read()
-text_out = text_in
+def do_headers(text_in):
+	text_out = text_in 
 
-def do_lists_tables(text_in):
-	text_out = text_in
+	whole_header_pattern = re.compile('=+ [^=\n]+ =+') #TODO: modify this regex to make sure starts(^) and ends($) the line?? Because python treats the entire file as one line, this match only works for single-line files. I've modified the regex to just use the trailing newline.
+	all_headers = whole_header_pattern.finditer(text_out)
+	
+	header_text = re.compile('[\w\s]+') 
+	count_eq = re.compile('=+') 
 
-	#TRAC		any number of indents then * text
-	#REDMINE	any number of *
-	#e.g.		'  *'  == '***'
-	list_pat = re.compile('\n *\* .+')
-	lists = list_pat.findall(text_out)
-	for line in lists:
-		spc_n_asterisk = re.compile(' *\*').search(line[1:]).group()
-		spcs = spc_n_asterisk[:len(spc_n_asterisk)-1]
-		space_count = spcs.count(' ')
-		list_item = line[space_count+2:] #the one removes the original asterisk
-		redmine_line = '\n*'+('*'*space_count)+list_item
-		text_out = text_out.replace(line, redmine_line)
+	for header in all_headers:
+		cur_hd_txt = header_text.search(header.group()) #should only be one set of text per header
+		cur_eq_list = count_eq.findall(header.group())
+		eq_num1 = len(cur_eq_list[0])
+		eq_num2 = len(cur_eq_list[1])
+		if eq_num1 != eq_num2:
+			print "Equal signs don't match in current header: '"+header+"'"#throw error
+			continue #just ignore current header, don't translate
+		new_hd = "h"+str(eq_num1)+"."+cur_hd_txt.group().rstrip()#rstrip gets rid of our trailing space
+		#It appears there's a glitch with header.start(), it's matching '= header_text ===' instead of '=== header_text ===' and therefore .start() returns 2 spaces before our full match begins. Also does the same with 4 equal signs.
+		#before = text_in[:(header.span()[0])]
+		#after = text_in[(header.span()[1]):]
+		#text_in = before+new_hd+after #replacing
+		
+		#The following line is a hacky band-aid for it.
+		text_out = text_out.replace(header.group(),new_hd) 
+
 	return text_out
 
-print do_lists_tables(text_out)
+text_in = open("pages_in_wiki/ABySS",'r').read()
+text_out = text_in
+
+print do_headers(text_out)
--- wiki_translate.py
+++ wiki_translate.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python
 
-import re, sys
+import re
 
 def do_headers(text_in):
-	#We're going to handle each header individually then once we have a list of original/TRAC headers and new/Redmine headers, we'll use re.sub('r...) to replace the old with the new.
-	text_out = text_in #just in-case, let's not destroy our original text_in
+	text_out = text_in 
 
-	whole_header_pattern = re.compile('(=+) [\w]+ (=+)\n') #TODO: modify this regex to make sure starts(^) and ends($) the line?? Because python treats the entire file as one line, this match only works for single-line files. I've modified the regex to just use the trailing newline.
+	whole_header_pattern = re.compile('=+ [^=\n]+ =+') #TODO: modify this regex to make sure starts(^) and ends($) the line?? Because python treats the entire file as one line, this match only works for single-line files. I've modified the regex to just use the trailing newline.
 	all_headers = whole_header_pattern.finditer(text_out)
 	
-	header_text = re.compile('[\w\s]+') #we'll use this to extract header text
-	count_eq = re.compile('=+') #we'll use this to count equal signs
+	header_text = re.compile('[\w\s]+') 
+	count_eq = re.compile('=+') 
 
 	for header in all_headers:
 		cur_hd_txt = header_text.search(header.group()) #should only be one set of text per header
-		#verify there was a match
 		cur_eq_list = count_eq.findall(header.group())
-		#verify they're of same length, else, syntax error from input :(
 		eq_num1 = len(cur_eq_list[0])
 		eq_num2 = len(cur_eq_list[1])
 		if eq_num1 != eq_num2:
 			print "Equal signs don't match in current header: '"+header+"'"#throw error
 			continue #just ignore current header, don't translate
-		new_hd = "h"+str(eq_num1)+"."+cur_hd_txt.group().rstrip()+"\n"#rstrip gets rid of our trailing space
+		new_hd = "h"+str(eq_num1)+"."+cur_hd_txt.group().rstrip()#rstrip gets rid of our trailing space
 		#It appears there's a glitch with header.start(), it's matching '= header_text ===' instead of '=== header_text ===' and therefore .start() returns 2 spaces before our full match begins. Also does the same with 4 equal signs.
 		#before = text_in[:(header.span()[0])]
 		#after = text_in[(header.span()[1]):]
